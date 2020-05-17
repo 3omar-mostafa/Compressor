9c632e7696fd23fd0a5b932e4dd47b23383dcb2d
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-05-18 00:24:34 +0200

    Adding Reading/Writing Raw Binary Data (unsigned strings)

--- BinaryIO.h
+++ BinaryIO.h
@@ -8,6 +8,8 @@ using namespace std;
 
 namespace BinaryIO {
 
+    typedef basic_string<unsigned char> ustring;
+
     string readBinaryFile(const string& filename) {
         ifstream input(filename, ios::in | ios::binary);
         string fileData;
@@ -21,12 +23,36 @@ namespace BinaryIO {
         return fileData;
     }
 
+    ustring readUnsignedBinaryFile(const string& filename) {
+        ifstream input(filename, ios::in | ios::binary);
+        ustring fileData;
+        if (input) {
+            input.seekg(0, ios::end);
+            fileData.resize(input.tellg());
+            input.seekg(0, ios::beg);
+            input.read((char*) (&fileData[0]), fileData.size());
+            input.close();
+        }
+        return fileData;
+    }
+
+
     // Append binaryData to the end of the file for strings
     void writeBinaryFile(const string& filename, const string& binaryData) {
 
         ofstream output(filename, ios::out | ios::binary | ios::app);
 
         output.write(binaryData.c_str(), binaryData.length());
+
+        output.close();
+    }
+
+    // Append binaryData to the end of the file for unsigned strings
+    void writeBinaryFile(const string& filename, const ustring& binaryData) {
+
+        ofstream output(filename, ios::out | ios::binary | ios::app);
+
+        output.write((char*) binaryData.c_str(), binaryData.length());
 
         output.close();
     }

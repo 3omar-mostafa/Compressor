e82f36a3f66c0bfcb81b696c246e642025a8e45d
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-05-18 10:27:19 +0200

    Added Move To Front (MFT) Encoder

---
+++ MTF.h
@@ -0,0 +1,76 @@
+#ifndef MTF_H
+#define MTF_H
+
+#include <list>
+#include <string>
+#include "BinaryIO.h"
+
+/**
+ * Wrapper around std::list::iterator
+ */
+struct Iterator {
+    list<unsigned char>::const_iterator iterator;
+    unsigned char index;
+    unsigned char value;
+
+    Iterator(const list<unsigned char>::const_iterator &iterator , unsigned int index) {
+        this->iterator = iterator;
+        this->index = index;
+        this->value = *iterator;
+    }
+};
+
+
+/**
+ * Move To Front Algorithm
+ */
+class MTF {
+
+    typedef basic_string<unsigned char> ustring;
+
+    list<unsigned char> symbolsList;
+
+public:
+
+    void encode(const string &filename , const string &outputFileName) {
+
+        ustring toBeEncoded = BinaryIO::readUnsignedBinaryFile(filename);
+        remove(outputFileName.c_str()); // remove the file if exists
+
+        generateSymbols();
+
+        ustring encoded;
+
+        for (unsigned char symbol : toBeEncoded) {
+            auto ptr = getIndexOfValue(symbol);
+            encoded += ptr.index;
+            symbolsList.erase(ptr.iterator);
+            symbolsList.push_front(symbol);
+        }
+
+        BinaryIO::writeBinaryFile(outputFileName , encoded);
+
+    }
+
+private:
+
+    void generateSymbols() {
+        for (unsigned int i = 0 ; i < 256 ; ++i) {
+            symbolsList.push_back(i);
+        }
+    }
+
+    Iterator getIndexOfValue(unsigned char c) {
+        unsigned int index = 0;
+        for (auto iter = symbolsList.begin() ; iter != symbolsList.end() ; ++iter) {
+            if (*iter == c)
+                return {iter , index};
+            index++;
+        }
+    }
+
+
+};
+
+
+#endif //MTF_H
